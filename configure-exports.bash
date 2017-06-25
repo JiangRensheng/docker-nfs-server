cfc9bed7e8855f2d4bde7ae8971a23044309a918
JiangRensheng <13841495@qq.com>
2017-06-25 06:11:21 +0000

    dm device supported

--- configure-exports.bash
+++ configure-exports.bash
@@ -3,13 +3,27 @@ NFS_EXPORT_DIRS=$(compgen -A variable|grep NFS_EXPORT_DIR)
 echo 'configure-exports called!'
 for dir in $NFS_EXPORT_DIRS
 do
+
  index=${dir##*_}
 
  net=NFS_EXPORT_DOMAIN_$index
  opt=NFS_EXPORT_OPTIONS_$index
 
  if [[ -n ${!dir} ]] && [[ -n ${!net} ]] && [[ -n ${!opt} ]] ; then
-  echo ${!dir} ${!net}'('${!opt}')'
+
+  export_dir="${!dir}"
+
+  if [ x"$(ls ${!dir} -l | awk '{print $4}')" == x"disk" ]; then
+   folder="$(basename ${!dir})"
+   export_dir="/mnt/${folder}"
+   echo "make dir : $export_dir"
+   [ -d "${export_dir}" ] || mkdir "${export_dir}"
+   mount "${!dir}" "${export_dir}"
+   echo ${!export_dir} ${!net}'('${!opt}')'
+  else
+   echo ${!dir} ${!net}'('${!opt}')'
+  fi
+
  fi
 
 done>/etc/exports 
